a39eb844aad22368c6d171cd34ed11ec114f8b55
itsjustbrian <bri.ferch@gmail.com>
2020-03-16 16:38:10 -0400

    allow zero backers

--- fundraising-backers.php
+++ fundraising-backers.php
@@ -1,6 +1,6 @@
 <?php
   $numBackers = get_field('number_of_backers');
-  if ($numBackers && is_numeric($numBackers)) {
+  if (is_numeric($numBackers)) {
     echo number_format($numBackers);
     echo $numBackers > 1 ? ' backers' : ' backer';
   }
