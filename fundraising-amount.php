a413dd2c8e74f15df78f7a966657d426fb009ace
Grant Mulitz <themulitzer@gmail.com>
2020-05-12 12:57:12 -0400

    removing number formatting to allow decimal inputs to fundraising fields

--- fundraising-amount.php
+++ fundraising-amount.php
@@ -1,3 +1,3 @@
 <?php if (is_numeric(get_field('amount_raised')) && is_numeric(get_field('funding_goal'))): ?>
-  <?php echo '$' . number_format(get_field('amount_raised')); ?><span> raised of <?php echo '$' . number_format(get_field('funding_goal')); ?></span>
+  <?php echo '$' . get_field('amount_raised'); ?><span> raised of <?php echo '$' . get_field('funding_goal'); ?></span>
 <?php endif; ?>
